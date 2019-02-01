9df9c07bd8d27e453495e35ccfbaa2164596a4ca
hayabusa <hayabusa@hs-sh.net>
2019-02-01 16:21:01 +0900

    設定ファイルにまだ存在しないキーを呼びだした時に、作成するように

--- main.py
+++ main.py
@@ -5,26 +5,18 @@ from mcrcon import MCRcon
 class Config:
     file = "config.ini"
 
-    def __init__(self):
+    def load(self, section, key, *, default=""):
         config = configparser.ConfigParser()
-        if not config.read(self.file):
-            config["Minecraft"] = {
-                "host": "localhost",
-                "port": 25575,
-                "password": "password",
-            }
-            with open(self.file, 'w') as configfile:
-                config.write(configfile)
 
-    def load(self, section, key):
-        config = configparser.ConfigParser()
         config.read(self.file)
 
         if not section in config:
             config[section] = {}
+            print("Config: [", section, "]")
 
         if not key in config[section]:
-            config[section][key] = ""
+            config[section][key] = default
+            print("Config: [", section, "][", key, "] : ", default)
             with open(self.file, 'w') as configfile:
                 config.write(configfile)
 
@@ -34,12 +26,14 @@ class Config:
 class Server:
     def __init__(self):
         conf = Config()
-        self.host = conf.load("Minecraft", "host")
-        self.password = conf.load("Minecraft", "password")
-        self.port = int(conf.load("Minecraft", "port"))
-        self.gce_zone = conf.load("GCE", "gce_zone")
-        self.gce_name = conf.load("GCE", "gce_name")
-        self.gcloud = conf.load("GCE", "gcloud")
+        self.host = conf.load("Minecraft", "host", default="localhost")
+        self.password = conf.load("Minecraft", "password", default="password")
+        self.port = conf.load("Minecraft", "port", default="25575")
+        self.gce_zone = conf.load("GCE", "gce_zone", default="asia-northeast1-b")
+        self.gce_name = conf.load("GCE", "gce_name", default="minecraft")
+        self.gcloud = conf.load("GCE", "gcloud", default="/snap/bin/gcloud")
+
+        self.port = int(self.port)
 
     def start(self):
         subprocess.run([self.gcloud, "compute", "instances", "start", "--zone", self.gce_zone, self.gce_name])
